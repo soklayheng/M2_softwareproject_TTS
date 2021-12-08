9a33c6e7981a23c5152af625b001e9bc11472e04
amillert <devmood.business@gmail.com>
2021-12-08 22:04:39 +0100

    add cors to flask server

--- src/server.py
+++ src/server.py
@@ -1,21 +1,25 @@
 from flask import Flask, request, jsonify
-
+from flask_cors import CORS, cross_origin
 
 from classifier import classify
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
+@cross_origin(origin="*", headers=["Content-Type", "Authorization"])
 def hello_world():
     return "<p>Server reachable!</p>"
 
 
 @app.route("/synthesize", methods=["GET"])
+@cross_origin(origin="*", headers=["Content-Type", "Authorization"])
 def synthesize():
     if request.method == "GET":
-        content = request.json
-        sentence = content["sentence"]
+        sentence = request.args.get("sentence")
+        # content = request.json
+        # sentence = content["sentence"]
 
         print(f"Processing: {sentence} in progress...")
         lang = classify(sentence)
@@ -26,4 +30,4 @@ def synthesize():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
