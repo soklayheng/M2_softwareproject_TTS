c8c5116614a352ff809012ad859141efa64361f0
amillert <devmood.business@gmail.com>
2021-12-09 19:34:11 +0100

    Temp change in ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,50 @@
-import React, {Component} from 'react'
-import Header from './Header'
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
-// Import here all the required components used by the router such as SignIn, Register, ...
+function App() {
+    const [speech, setSpeech] = useState("");
 
-class App extends Component {
-    render(){
-        return(
-           <Router>
-               <div>
-                   <Header />
-                   <Route exact path="/" component={Home}/>
-                   <Route path="/about" component={About}/>
-                   <Route path="/contact" component={Contact}/>
-               </div>
-           </Router>
-        )
-    }
+    useEffect(() => {
+        fetch("/synthesize", {
+            method: "POST",
+            headers: { "Content-Type": "application/json", "Authorization": "Bearer my-token" },
+            body: JSON.stringify({ sentence: "Jakie to zdanie" })
+        }
+        ).then(res => res.json()).then(data => {
+            setSpeech(data.speech);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <p>Response is {speech}</p>
+        </div>
+    );
 }
-export default App
+
+export default App;
+
+// import React, {Component} from 'react'
+// import Header from './Header'
+// import Home from './Home'
+// import About from './About'
+// import Contact from './Contact'
+// import { BrowserRouter as Router, Route } from 'react-router-dom'
+// 
+// // Import here all the required components used by the router such as SignIn, Register, ...
+// 
+// class App extends Component {
+//     render(){
+//         return(
+//            <Router>
+//                <div>
+//                    <Header />
+//                    <Route exact path="/" component={Home}/>
+//                    <Route path="/about" component={About}/>
+//                    <Route path="/contact" component={Contact}/>
+//                </div>
+//            </Router>
+//         )
+//     }
+// }
+// export default App
